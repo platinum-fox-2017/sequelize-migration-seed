60b72eb6af8e524e283247402c04c0a0d96d563b
haripermadi <harinurpermadi@gmail.com>
2018-02-22 14:58:01 +0700

    release 2

--- views/contact.js
+++ views/contact.js
@@ -2,6 +2,21 @@ class View_contact{
   constructor(){
 
   }
+  static showAll(dataAll){
+    console.log(dataAll)
+  }
+  static showErr(err){
+    console.log(err)
+  }
+  static showAdd(dataAdd){
+    console.log(dataAdd)
+  }
+  static showUpdate(id){
+    console.log(`Data dengan id : ${id} berhasil diupdate`)
+  }
+  static showDelete(id){
+    console.log(`Data dengan id: ${id} berhasil didelete`)
+  }
 
 }
 
