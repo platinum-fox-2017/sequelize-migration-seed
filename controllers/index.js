3c8fba02b1958b5d739a24b35103a8a3b2edcbe8
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-22 14:07:21 +0700

    Release 2 : Crud Addresses

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
 const ContactController = require('./contact');
+const AddressController = require('./address');
+
 
 class MainController {
   static routeCommand(command, options){
     if (command.search('contacts') >= 0) {
       ContactController.main(command,options);
+    } else if (command.search('addresses') >= 0) {
+      AddressController.main(command,options);
     }
   }
 }
