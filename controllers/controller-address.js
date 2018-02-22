e130329c87ffcca772bd0e61beb4adde1dd701dc
yohanessahrul <yohanessahrul92@gmail.com>
2018-02-22 20:58:41 +0700

    migration-seed

---
+++ controllers/controller-address.js
@@ -0,0 +1,10 @@
+class ControllerAddresses {
+    constructor(){
+
+    }
+    static manageAction(act, value){
+        console.log('ini controller Addresses')
+    }
+}
+
+module.exports = ControllerAddresses
