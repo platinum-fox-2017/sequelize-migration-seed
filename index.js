77022563ac00555a0214ae21d327f07c9ccca100
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-22 14:25:44 +0700

    Release 2

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// node index.js contacts:update id column value
-// node index.js contacts:delete id
-
 const ContactControllers = require('./controllers/contact')
+const AddressControllers = require('./controllers/address')
 
 if (process.argv[2] === 'contacts:list') {
     ContactControllers.showAll()
@@ -11,6 +9,14 @@ if (process.argv[2] === 'contacts:list') {
     ContactControllers.updateContact(process.argv[3], process.argv[4].split(','))
 } else if (process.argv[2] === 'contact:delete') {
     ContactControllers.deleteContact(process.argv[3])
+} else if (process.argv[2] === 'addresses:list') {
+    AddressControllers.showAll()
+} else if (process.argv[2] === 'addresses:add') {
+    AddressControllers.addAddress(process.argv[3], process.argv[4], process.argv[5])
+} else if (process.argv[2] === 'addresses:update') {
+    AddressControllers.updateAddress(process.argv[3], process.argv[4].split(','))
+} else if (process.argv[2] === 'addresses:delete') {
+    AddressControllers.deleteAddress(process.argv[3])
 }
 
 
